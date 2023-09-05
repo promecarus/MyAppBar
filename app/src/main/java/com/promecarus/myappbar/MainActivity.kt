8c3daa31eece222716bc14ac94f27c6a4875e5c5
promecarus <haikalslipi@gmail.com>
2023-09-05 22:46:35 +0700

    part01

--- app/src/main/java/com/promecarus/myappbar/MainActivity.kt
+++ app/src/main/java/com/promecarus/myappbar/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.promecarus.myappbar
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.promecarus.myappbar.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.menu1 -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, MenuFragment())
+                        .addToBackStack(null)
+                        .commit()
+                    true
+                }
+
+                R.id.menu2 -> {
+                    val intent = Intent(this, MenuActivity::class.java)
+                    startActivity(intent)
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
